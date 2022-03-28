cb990ddfc4c0f20263330b6decc7c45e84c448f5
Dorian Guilmain <guilmain.dorian@gmail.com>
2022-03-28 11:16:50 +0200

    :sparkles: Update selector to catch  smaller values

--- src/api/dexscreener.js
+++ src/api/dexscreener.js
@@ -1,21 +1,26 @@
 import puppeteer from 'puppeteer'
 
+const DEVICE_WIDTH = 1920
+const DEVICE_HEIGHT = 1080
+const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0'
+
 export const getLastUsdValue = async ({ url }) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
+  await page.setCacheEnabled(false)
+  await page.setViewport({ width: DEVICE_WIDTH, height: DEVICE_HEIGHT })
+  await page.setUserAgent(USER_AGENT)
+
   await page.goto(url)
 
-  await page.waitForSelector('.chakra-link.chakra-wrap__listitem', {
-    visible: true,
-  })
+  await page.waitForSelector('body tbody')
 
   const bodyHandle = await page.$('body')
 
   const innerText = await page.evaluate((body) => {
-    return Array.from(body.querySelectorAll('span')).find(
-      (el) => el.textContent === 'Price USD',
-    ).nextElementSibling.innerText
+    return body.querySelector('tbody tr > td.chakra-text:nth-child(6)').innerText
   }, bodyHandle)
 
   await browser.close()
